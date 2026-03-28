ad30c5f6da9166c6d8bf6ab612186ba7c86b2d69
gabbar sharma <gabbarsharma75663@gmail.com>
2026-03-28 20:35:11 +0530

    adding a function

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.get('/about', (req, res) => {
   res.send('About Us')
 })
 
+
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+  console.log('Server is running on localhost')
 })
