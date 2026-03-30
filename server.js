67190c1de945da5ddbd3a6f72f6975c5f2779637
gabbar sharma <gabbarsharma75663@gmail.com>
2026-03-30 22:24:44 +0530

    adding a post jwt

--- server.js
+++ server.js
@@ -25,21 +25,6 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model("User", userSchema);
 
 // REGISTER
-app.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const user = new User({
-    name,
-    email,
-    password: hashedPassword
-  });
-
-  await user.save();
-
-  res.json({ message: "User Registered ✅" });
-});
 
 // LOGIN
 app.post("/login", async (req, res) => {
